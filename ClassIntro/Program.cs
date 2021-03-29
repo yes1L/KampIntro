8721b98bd53036d28c9731542472a2abfc03a068
Mustafa YEŞİL <mustafayesil518@gmail.com>
2021-03-29 14:52:10 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Mustafa";
+            int yas = 22;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.KursunEgitmeni = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.KursunEgitmeni = "Kerem Varış";
+            kurs2.IzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Python";
+            kurs3.KursunEgitmeni = "Berkay Bilgin";
+            kurs3.IzlenmeOrani = 80;
+
+
+
+
+            //Console.WriteLine(kurs1.KursAdi+" : "+kurs1.KursunEgitmeni);
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.KursunEgitmeni);
+            }
+ 
+            
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string KursunEgitmeni { get; set; }
+        public int IzlenmeOrani { get; set; }
+
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya Baişangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", 
+                "Programlamaya Baişangıç İçin Temel Kurs", 
+                "Java", "Python", "C++" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
