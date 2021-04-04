5f48084d069815db9d8649c0a782b683dd78baf0
Mustafa YEŞİL <mustafayesil518@gmail.com>
2021-04-04 14:15:19 +0300

    5. Gün Ders sonu

---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Coorporate
+    //Miras - inheritance
+    class TuzelMusteri:Musteri
+    {
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
